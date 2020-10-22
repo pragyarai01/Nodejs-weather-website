752ff09d07168355ffc7c2be1f1f59ee0117d503
Pragya <prags012@gmail.com>
2020-10-22 11:59:50 +0530

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@ weatherForm.addEventListener('submit',(e) => {
     message2.textContent = ''
 
     console.log(location)
-    const url = 'http://localhost:3000/weather?address=' + location
+    const url = '/weather?address=' + location
     fetch(url)
     .then((response) => {
         response.json().then((data) => {
